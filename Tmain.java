5386b77bee1e718a288168b1f951e9b9bb967e2d
kgwSpace <115144167+kgwSpace@users.noreply.github.com>
2022-10-20 17:31:14 +0900

    Update Tmain.java

--- Tmain.java
+++ Tmain.java
@@ -3,14 +3,23 @@ public class Tmain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		HPPrinter hpPrn = new HPPrinter();
-		SSPrinter ssPrn = new SSPrinter();
+		//HPPrinter hpPrn = new HPPrinter();
+		//SSPrinter ssPrn = new SSPrinter();
+		Printable[] prns = new Printable[2];
 		
-		hpPrn.initPrinter();
-		hpPrn.printBlack("문서");
 		
-		ssPrn.initPrinter();
-		ssPrn.printBlack("문서");
+		prns[0] = new HPPrinter();
+		prns[1] = new SSPrinter();
+		
+		//hpPrn.initPrinter();
+		//ssPrn.initPrinter();
+		prns[0].initPrinter();
+		prns[1].initPrinter();
+		
+		//hpPrn.printBlack("문서");
+		//ssPrn.printBlack("문서");
+		prns[0].printBlack("문서");
+		prns[1].printBlack("문서");
 	}
 
 }
